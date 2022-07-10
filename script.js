c45b5cdfe1b7c17f456fa55188d1e7c1a354a779
Soo <soowjo@gmail.com>
2022-07-10 14:32:29 +0900

    Update 16x16 grid

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function makeGrid (size) {
     grid.style['grid-template-columns'] = `repeat(${size}, 1fr)`
     grid.style['grid-template-rows'] = `repeat(${size}, 1fr)`
     for (let i=0; i<size**2; i++) {
-        const gridCell = document.createElement('div');
-        grid.appendChild(gridCell);
+        const cell = document.createElement('div');
+        grid.appendChild(cell;
     }
 }
 
